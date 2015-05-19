668eba15708885ac0279b4d97de01d46c31aaabd
Martin Angers <martin.n.angers@gmail.com>
2015-05-19 19:50:20 -0400

    add -A, -B, -C flags (small bug when a match is found inside the context lines)

--- src/cmd/trouve.cr
+++ src/cmd/trouve.cr
@@ -5,6 +5,8 @@ module Trouve
   VERSION = "0.0.1"
 end
 
+before, after = 0, 0
+
 opts = OptionParser.parse! do |opts|
     opts.banner = "Usage: #{PROGRAM_NAME} [OPTION]... PATTERN [FILES OR DIRECTORIES]
 
@@ -13,6 +15,15 @@ directory or in the specified FILES or DIRECTORIES."
 
     opts.on("-h", "--help", "print this help menu") do
         puts opts
+    end
+    opts.on("-B NUM", "--before", "contextual lines before the match") do |val|
+        before = val.to_i
+    end
+    opts.on("-A NUM", "--after", "contextual lines after the match") do |val|
+        after = val.to_i
+    end
+    opts.on("-C NUM", "--context", "contextual lines before and after the match") do |val|
+        after = before = val.to_i
     end
 end
 
@@ -25,5 +36,7 @@ when 1
 else
     job = Trouve::Job.new(ARGV[0], ARGV[1])
 end
+job.after = after
+job.before = before
 
 job.run
